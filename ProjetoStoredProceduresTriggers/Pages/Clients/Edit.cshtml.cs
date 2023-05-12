041b360975249e27c22c5ed79b1780f1026af351
Marcos Rigel <marcosrigel99@gmail.com>
2023-05-12 18:14:35 -0300

    codigo versao 1.2

---
+++ ProjetoStoredProceduresTriggers/Pages/Clients/Edit.cshtml.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace ProjetoStoredProceduresTriggers.Pages.Clients
+{
+    public class EditModel : PageModel
+    {
+        public ClientInfo clientInfo = new ClientInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
+
+        public void OnGet()
+        {
+            String id = Request.Query["id"];
+
+            try
+            {
+                String connectionString = "Data Source=RIGEL\\SQLSERVER2022;Initial Catalog=mystore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients WHERE id=@id";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("id", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+
+        public void OnPost()
+        {
+            clientInfo.id = Request.Form["id"];
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+            if(clientInfo.id.Length == 0 || clientInfo.name.Length == 0 ||
+                clientInfo.email.Length == 0 || clientInfo.phone.Length == 0 ||
+                clientInfo.address.Length == 0)
+            {
+                errorMessage = "All fields are required";
+                return;
+            }
+
+            try
+            {
+                String connectionString = "Data Source=RIGEL\\SQLSERVER2022;Initial Catalog=mystore;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO clients " +
+                                 "(name, email, phone, address) VALUES " +
+                                 "(@name, @email, @phone, @address);";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue("@email", clientInfo.email);
+                        command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+
+            Response.Redirect("/Clients/Index");
+        }
+    }
+}
